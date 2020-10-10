8b38522a57496f12b70999260d86791da24a3a76
amaleshuk <amaleshuk14@gmail.com>
2020-10-10 19:57:52 +0530

    latest

--- Form2.cs
+++ Form2.cs
@@ -13,13 +13,14 @@ using System.Drawing.Text;
 
 namespace BakeryBilling
 {
+
     public partial class frmBill : Form
     {
         public frmBill()
         {
             InitializeComponent();
         }
-        class Items
+        public class Items
         {
             public int id { get; set; }
             public string name { get; set; }
@@ -29,9 +30,12 @@ namespace BakeryBilling
             public double tot_mrp { get; set; }
             public double tot_sprice { get; set; }
         }
+
+
+        string billid = "0";
         List<Items> itemList = new List<Items>();
         OleDbConnection conn;
-        OleDbCommand billCmd,productCmd;
+        OleDbCommand billCmd, productCmd;
         OleDbDataAdapter billAdapter, productAdapter;
         DataTable billTable, productTable;
         double total_mrp, total_sprice;
@@ -44,16 +48,16 @@ namespace BakeryBilling
                 total_mrp += item.tot_mrp;
                 total_sprice += item.tot_sprice;
             }
-            lbl_totalmrp.Text = String.Format("{0:0.00}",total_mrp); 
+            lbl_totalmrp.Text = String.Format("{0:0.00}", total_mrp);
             lbl_ourtotal.Text = String.Format("{0:0.00}", total_sprice);
         }
 
         private void cmb_items_SelectionChangeCommitted(object sender, EventArgs e)
         {
-            if (cmb_items.SelectedValue!=null)
+            if (cmb_items.SelectedValue != null)
             {
                 var id = cmb_items.SelectedValue;
-                
+
                 txt_id.Text = "0";
                 txt_mrp.Text = "";
                 txt_sprice.Text = "";
@@ -86,8 +90,8 @@ namespace BakeryBilling
                     MessageBox.Show(ex.Message);
                 }
             }
-            
-            
+
+
 
 
         }
@@ -119,11 +123,11 @@ namespace BakeryBilling
                 }
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show("No Selections Made");
             }
-            
+
 
 
 
@@ -133,46 +137,65 @@ namespace BakeryBilling
         int itemPerPage = 0;
         private void Bill_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
         {
-            
-            int paperWidth = 1122;
-            int x = 30;
+
+
+
+            int x = 10;
             int y = 10;
             Pen pen = new Pen(Brushes.Black);
             pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
-            
-            int centerX = (int)(x + Math.Abs(paperWidth * 0.5));
-            
-            Font H1 = new Font("Arial", 28);
-            Font H2 = new Font("Arial", 16);
-            Font H3 = new Font("Arial", 12);
-            e.Graphics.DrawString("LEOS BAKERY & PROVISION STORE ",H1,Brushes.Black,new Point(x+80,y));
-            y = y + 50;
-            e.Graphics.DrawLine(pen, new Point(0, y), new Point(1000, y));
-            y = y + 10;
-            e.Graphics.DrawString("Item", H2, Brushes.Black, new Point(x, y));
-            e.Graphics.DrawString("QTY", H2, Brushes.Black, new Point(x+100, y));
-            e.Graphics.DrawString("MRP", H2, Brushes.Black, new Point(x + 200, y));
-            e.Graphics.DrawString("OurPrice", H2, Brushes.Black, new Point(x + 300, y));
-            e.Graphics.DrawString("Tot MRP", H2, Brushes.Black, new Point(x + 450, y));
-            e.Graphics.DrawString("Tot Our Price", H2, Brushes.Black, new Point(x + 620, y));
+
+            int centerX = (int)(x + Math.Abs(285 * 0.5));
+
+            Font H1 = new Font("Arial", 8, FontStyle.Bold);
+            Font H2 = new Font("Arial", 8);
+            Font H3 = new Font("Arial", 5, FontStyle.Bold);
+            Font H4 = new Font("Arial", 5);
+            Font th = new Font("Arial", 6);
+            StringFormat sf = new StringFormat();
+
+            sf.Alignment = StringAlignment.Center;
+
+
+            e.Graphics.DrawString("LEOS BAKERY & PROVISION STORE ", H1, Brushes.Black, new Point(x + 98, y), sf);
+            y = y + 15;
+            e.Graphics.DrawString("Thiruvamkulam Temple Road.\nMob 9744678214 ", H2, Brushes.Black, new Point(x + 98, y), sf);
+            y = y + 40;
+            e.Graphics.DrawString("Bill." + billid, H2, Brushes.Black, new Point(x, y));
+            e.Graphics.DrawString("Date." + DateTime.Today.ToShortDateString(), H2, Brushes.Black, new Point(x + 110, y)); ;
+
             y = y + 30;
+            e.Graphics.DrawString("S.No", H3, Brushes.Black, new Point(x, y));
+            e.Graphics.DrawString("ITM", H3, Brushes.Black, new Point(x + 30, y));
+            e.Graphics.DrawString("QTY", H3, Brushes.Black, new Point(x + 80, y));
+            e.Graphics.DrawString("MRP", H3, Brushes.Black, new Point(x + 110, y));
+            e.Graphics.DrawString("ORP", H3, Brushes.Black, new Point(x + 130, y));
+            e.Graphics.DrawString("TOT.ORP", H3, Brushes.Black, new Point(x + 160, y));
+            y = y + 10;
             e.Graphics.DrawLine(pen, new Point(0, y), new Point(1000, y));
-            y = y + 20;
-            while(listIndex<itemList.Count)
+            y = y + 5;
+            //collect bill items
+            billCmd = new OleDbCommand("SELECT * FROM BILLED_ITEMS WHERE BILL_ID=" + billid, conn);
+            billAdapter = new OleDbDataAdapter();
+            billTable = new DataTable();
+            billAdapter.SelectCommand = billCmd;
+            billAdapter.Fill(billTable);
+
+            while (listIndex < billTable.Rows.Count)
             {
-               
-                e.Graphics.DrawString(itemList[listIndex].name, H3, Brushes.Black, new Point(x, y));
-                e.Graphics.DrawString(itemList[listIndex].qty.ToString(), H3, Brushes.Black, new Point(x + 110, y));
-                e.Graphics.DrawString(itemList[listIndex].mrp.ToString(), H3, Brushes.Black, new Point(x + 210, y));
-                e.Graphics.DrawString(itemList[listIndex].sprice.ToString(), H3, Brushes.Black, new Point(x + 340, y));
-                e.Graphics.DrawString(itemList[listIndex].tot_mrp.ToString(), H3, Brushes.Black, new Point(x + 480, y));
-                e.Graphics.DrawString(itemList[listIndex].tot_sprice.ToString(), H3, Brushes.Black, new Point(x + 650, y));
-                y = y + 30;
+                DataRow row = billTable.Rows[listIndex];
+                e.Graphics.DrawString((listIndex + 1).ToString(), H4, Brushes.Black, new Point(x, y));
+                e.Graphics.DrawString(row["ITEM_NAME"].ToString(), H4, Brushes.Black, new Point(x + 30, y));
+                e.Graphics.DrawString(row["BILL_QTY"].ToString(), H4, Brushes.Black, new Point(x + 80, y));
+                e.Graphics.DrawString(((double)row["BILL_MRP"] / (double)row["BILL_QTY"]).ToString(), H4, Brushes.Black, new Point(x + 110, y));
+                e.Graphics.DrawString(((double)row["BILL_SPRICE"] / (double)row["BILL_QTY"]).ToString(), H4, Brushes.Black, new Point(x + 130, y));
+                e.Graphics.DrawString(row["BILL_SPRICE"].ToString(), H4, Brushes.Black, new Point(x + 160, y));
+                y = y + 10;
                 if (itemPerPage < 25)
                 {
                     e.HasMorePages = false;
                     itemPerPage += 1;
-                    
+
                 }
                 else
                 {
@@ -186,25 +209,29 @@ namespace BakeryBilling
 
             }
             e.Graphics.DrawLine(pen, new Point(0, y), new Point(1000, y));
-            y = y + 20;
-            e.Graphics.DrawString("Total MRP", H2, Brushes.Black, new Point(x, y));
-            e.Graphics.DrawString(total_mrp.ToString(), H2, Brushes.Black, new Point(x+120, y));
-            e.Graphics.DrawString("Our Total Price", H2, Brushes.Black, new Point(x+200, y));
-            e.Graphics.DrawString(total_sprice.ToString(), H2, Brushes.Black, new Point(x + 350, y));
-            y = y + 30;
+            y = y + 10;
+            e.Graphics.DrawString("Total MRP", H3, Brushes.Black, new Point(x, y));
+            e.Graphics.DrawString(total_mrp.ToString(), H3, Brushes.Black, new Point(x + 40, y));
+            e.Graphics.DrawString("Our Total Price", H3, Brushes.Black, new Point(x + 60, y));
+            e.Graphics.DrawString(total_sprice.ToString(), H3, Brushes.Black, new Point(x + 115, y));
+
+            y = y + 10;
             e.Graphics.DrawLine(pen, new Point(0, y), new Point(1000, y));
-            y = y + 20;
-            e.Graphics.DrawString("You save Rs."+(total_mrp-total_sprice).ToString()+" ,Thank you for shopping with us!", H2, Brushes.Black, new Point(x+150, y));
-            
+            y = y + 10;
+            e.Graphics.DrawString("You save Rs." + (total_mrp - total_sprice).ToString() + " ,Thank you for shopping with us! ", th, Brushes.Black, new Point(x, y));
+            y = y + 10;
+
+            billCmd.Dispose();
+            billAdapter.Dispose();
+
 
         }
-
         private void btn_print_Click(object sender, EventArgs e)
         {
-            if(itemList.Count>0)
+            if (itemList.Count > 0)
             {
                 string date = dt_bill.Text;
-                string billid = "0";
+
                 //Create a new bill entry
 
                 try
@@ -229,16 +256,17 @@ namespace BakeryBilling
                     foreach (DataRow row in billTable.Rows)
                     {
                         billid = row["ID"].ToString();
+
                     }
                     billCmd.Dispose();
                     billAdapter.Dispose();
-                    billTable.Dispose();
+                    //billTable.Dispose();
 
                     //Add bill items to the bill items table
 
                     foreach (Items item in itemList)
                     {
-                        query = "INSERT INTO BILLED_ITEMS(BILL_ID,ITEM_ID,ITEM_NAME,BILL_MRP,BILL_SPRICE,BILL_QTY) VALUES(" + billid + ","+item.id+",'" + item.name + "'," + item.tot_mrp.ToString() + "," + item.tot_sprice.ToString() + "," + item.qty.ToString() + ")";
+                        query = "INSERT INTO BILLED_ITEMS(BILL_ID,ITEM_ID,ITEM_NAME,BILL_MRP,BILL_SPRICE,BILL_QTY) VALUES(" + billid + "," + item.id + ",'" + item.name + "'," + item.tot_mrp.ToString() + "," + item.tot_sprice.ToString() + "," + item.qty.ToString() + ")";
                         billCmd = new OleDbCommand(query, conn);
                         billAdapter = new OleDbDataAdapter();
                         billAdapter.InsertCommand = billCmd;
@@ -274,11 +302,9 @@ namespace BakeryBilling
 
                 listIndex = 0;
                 itemPerPage = 0;
-                PrintPreviewDialog printPreview = new PrintPreviewDialog();
-                printPreview.Document = Bill;
-                printPreview.Show();
+                
 
-                //Bill.Print();
+                Bill.Print();
 
                 refresh_product_combo();
             }
@@ -286,9 +312,9 @@ namespace BakeryBilling
             {
                 MessageBox.Show("Noting to print");
             }
-            
-            
-            
+
+
+
         }
 
         private void btn_cancel_Click(object sender, EventArgs e)
@@ -322,10 +348,10 @@ namespace BakeryBilling
         private void validate_number(object sender, KeyPressEventArgs e)
         {
             string txtsource = ((TextBox)sender).Name.ToString();
-            
-            if (txtsource == "txt_qty" || txtsource == "txt_mrp"|| txtsource=="txt_sprice")
+
+            if (txtsource == "txt_qty" || txtsource == "txt_mrp" || txtsource == "txt_sprice")
             {
-                if (e.KeyChar >= '0' && e.KeyChar <= '9' || e.KeyChar == 8 || e.KeyChar=='.'|| e.KeyChar =='.' )
+                if (e.KeyChar >= '0' && e.KeyChar <= '9' || e.KeyChar == 8 || e.KeyChar == '.' || e.KeyChar == '.')
                 {
                     e.Handled = false;
                 }
@@ -349,7 +375,7 @@ namespace BakeryBilling
 
         private void cmb_items_TextChanged(object sender, EventArgs e)
         {
-           
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -417,7 +443,7 @@ namespace BakeryBilling
             cmb_items.Focus();
         }
 
-        
+
 
 
         private void frmBill_Load(object sender, EventArgs e)
@@ -427,16 +453,16 @@ namespace BakeryBilling
             try
             {
                 conn.Open();
-                
+
                 refresh_product_combo();
 
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
-            
+
         }
         private void refresh_product_combo()
         {
@@ -459,16 +485,16 @@ namespace BakeryBilling
                 cmb_items.AutoCompleteSource = AutoCompleteSource.ListItems;
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
             cmb_items.SelectedIndexChanged += cmb_items_SelectedIndexChanged;
         }
-        
 
-            
-        
+
+
+
     }
 
 }
